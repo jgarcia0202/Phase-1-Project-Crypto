5be8fd8982367c0eb5e4214129a3cdc2d05bca9f
Jose <jmgarcia0202@gmail.com>
2022-01-27 05:31:46 -0800

    added an event listener domloaded

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-async function getCryto() {
-    const response = await fetch(cryptoUrl);
-    const dat = await response.json();
-    const symbArray= [];
-    for (var i = 0; i < 20; i++) {
-      const symbol = dat.data[i].symbol;
-      console.log(symbol);
-      symbArray = symbol.push()
-    }
-    document.getElementById('sym').textContent = symbArray;}
+let text = document.getElementById('text');
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    text.textContent = "This comes last";
+  });
