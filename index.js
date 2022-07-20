e7adc56161270559fb6274e4b217e675dfef2d41
Jose <jmgarcia0202@gmail.com>
2022-07-19 17:38:25 -0700

    added display of crypto and two new buttons (next 50 and back 50)

--- index.js
+++ index.js
@@ -6,25 +6,34 @@ const form = document.querySelector('form')
 const content = document.querySelector('#content')
 const buttons = document.querySelector('#buttons')
 let start = 0
+let count = document.querySelector('#count')
 
 document.addEventListener('DOMContentLoaded',() => {
     fetchCrypto()
-
 })
 
 const fetchCrypto = () =>{
-  fetch('https://api.coinlore.net/api/tickers/?start=0&limit=10')
+  fetch(`https://api.coinlore.net/api/tickers/?start=${start}&limit=10`)
   .then(response => response.json())
   .then(dat => dat.data)
   .then(cryptos => {
+    count.innerText = ` Displaying Cryptocurrencies Rank ${start} to ${start + 10}`
     cryptos.forEach(crypto => addToList(crypto))
+    let nextFityButton = document.createElement('button')
+    nextFityButton.className = 'butt2'
+    nextFityButton.innerText = ' NEXT [50] '
     let nextButton = document.createElement('button')
     nextButton.className = 'butt2'
     nextButton.innerText = ' NEXT '
     let backButton = document.createElement('button')
     backButton.className = 'butt1'
     backButton.innerText = ' BACK '
+    let backFityButton = document.createElement('button')
+    backFityButton.className = 'butt1'
+    backFityButton.innerText = ' BACK [50] '
+    buttons.appendChild(backFityButton)
     buttons.appendChild(backButton)
+    buttons.appendChild(nextFityButton)
     buttons.appendChild(nextButton)
      nextButton.addEventListener('click', e => {
       mainList.innerHTML = ' '
@@ -34,6 +43,14 @@ const fetchCrypto = () =>{
       .then(dat => dat.data)
       .then(cryptos => cryptos.forEach(crypto => addToList(crypto)))
   })
+  nextFityButton.addEventListener('click', e => {
+    mainList.innerHTML = ' '
+    start +=50
+    fetch(`https://api.coinlore.net/api/tickers/?start=${start}&limit=10`)
+    .then(response => response.json())
+    .then(dat => dat.data)
+    .then(cryptos => cryptos.forEach(crypto => addToList(crypto)))
+})
   backButton.addEventListener('click', e => {
       if (start == 0){
           alert('Cannot Go Back')
@@ -46,10 +63,23 @@ const fetchCrypto = () =>{
       .then(dat => dat.data)
       .then(cryptos => cryptos.forEach(crypto => addToList(crypto)))
   })
+  backFityButton.addEventListener('click', e => {
+    if (start <= 49){
+        alert('Cannot Go Back')
+        return
+    }
+    mainList.innerHTML = ' '
+    start -=50
+    fetch(`https://api.coinlore.net/api/tickers/?start=${start}&limit=10`)
+    .then(response => response.json())
+    .then(dat => dat.data)
+    .then(cryptos => cryptos.forEach(crypto => addToList(crypto)))
+})
   fillContent(cryptos[0])
 })}
 function addToList(crypto){
   //console.log(crypto)
+  count.innerText = ` Displaying Cryptocurrencies Rank ${start} to ${start + 10}`
   let card = document.createElement('ul')
   card.className = 'variable'
   card.innerHTML = ` ${crypto.symbol}`
@@ -69,7 +99,7 @@ const fillContent = (crypto) => {
     content.innerHTML = ''
     let card = document.createElement('ul')
     card.innerHTML = `
-    <h5> Crypto Selected: [${crypto.symbol}] </h5>
+    <h5> Stats Of [${crypto.symbol}]: </h5>
     <p> Name: [${crypto.name}], Market Cap: [${crypto.market_cap_usd}],
       Perecent Change Last 24 hrs: [${crypto.percent_change_24h}], Price: [$${crypto.price_usd}],
       Rank in Cryptocurrency: [${crypto.rank}], Supply of Coin: [${crypto.tsupply}]
